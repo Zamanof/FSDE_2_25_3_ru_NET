0f9a06589c1ee77828644ff41ff048a5570eaee7
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-06-05 22:03:31 +0400

    Lesson 01

--- NET 01. Data Types/Program.cs
+++ NET 01. Data Types/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ - Data Types
+    - Value Types
+       - struct
+       - create in stack (LIFO)
+       - create without new keyword
+       - compare by value
+       - copy value
+       - 
+    - Reference Types
+      - class
+      - create Heap
+      - create with new keyword
+      - compare by reference
+      - copy reference
+      - Garbage Collector
+        
+ 
+ 
+ */
+
